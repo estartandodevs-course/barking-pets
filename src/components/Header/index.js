80605e5d16f6dcdb9aebb68b567600d657518e6b
sergjun <serrgio.madureira@gmail.com>
2021-07-31 20:46:05 -0300

    tirado a tag que imagem que a header estava recebendo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,10 @@
-import patinha from "../../assets/img/patinha.png";
 import "./styles.scss";
 
 export function Header() {
   return (
     <>
       <header className="header">
-        <img src={patinha} alt="logo barking pets" className="header--logo" />
-        <h1 className="header--titulo">Barking Pets</h1>
+        <h1 className="header__titulo">Barking Pets</h1>
       </header>
     </>
   );
