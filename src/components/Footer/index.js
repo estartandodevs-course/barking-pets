5091fcebd5265762f06fe67e6f55991363b96adc
sergjun <serrgio.madureira@gmail.com>
2021-08-02 19:29:47 -0300

    finalizado o componente Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,32 @@
 import "./styles.scss";
+import facebook from "../../assets/icons/facebook-icon.svg";
+import instagram from "../../assets/icons/instagram-icon.svg";
+import Petlogo from "../../assets/img/Pet-Doodles.svg";
 
 export function Footer() {
   return (
     <>
       <footer className="footer">
-        <p className="footer__text">Entrar em contato</p>
-        <p className="footer__text">Barkingpets@hotmail.com</p>
-        <p className="footer__text">Redes Sociais:</p>
-        <img alt="." />
-
-        <img alt="." />
+        <div className="footer__text--left">
+          <p className="footer__text">Entrar em contato</p>
+          <p className="footer__text--email">Barkingpets@hotmail.com</p>
+        </div>
+        <div className="footer__text--right">
+          <p className="footer__text">Redes Sociais:</p>
+          <div className="footer__container">
+            <img
+              src={instagram}
+              alt="icone instagram"
+              className="footer__icons"
+            />
+            <img
+              src={facebook}
+              alt="icone facebook"
+              className="footer__icons--facebook"
+            />
+          </div>
+        </div>
+        <img src={Petlogo} alt="icone petlogo" />
       </footer>
     </>
   );
