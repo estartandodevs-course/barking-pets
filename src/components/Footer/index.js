a25cb78717c508caec9a14843157b12758a54850
sergjun <serrgio.madureira@gmail.com>
2021-08-01 21:20:31 -0300

    criado o componente footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import "./styles.scss";
+
+export function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <p className="footer__text">Entrar em contato</p>
+        <p className="footer__text">Barkingpets@hotmail.com</p>
+        <p className="footer__text">Redes Sociais:</p>
+        <img alt="." />
+
+        <img alt="." />
+      </footer>
+    </>
+  );
+}
