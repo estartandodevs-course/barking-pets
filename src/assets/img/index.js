a3aa953780bbba94d7ddf662eb22c3fdc3f2b29c
joaoprferreira <joaof082@gmail.com>
2021-08-03 20:46:30 -0300

    Exportando imagens do card
    Signed-off-by: joaoprferreira <joaof082@gmail.com>

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -1,3 +1,11 @@
 import MenuIcon from "./menu.svg";
 
 export { MenuIcon };
+export { default as pet1 } from "./pet1.svg";
+export { default as pet2 } from "./pet2.svg";
+export { default as pet3 } from "./pet3.svg";
+export { default as pet4 } from "./pet4.svg";
+export { default as pet5 } from "./pet5.svg";
+export { default as pet6 } from "./pet6.svg";
+export { default as pet7 } from "./pet7.svg";
+export { default as pet8 } from "./pet8.svg";
