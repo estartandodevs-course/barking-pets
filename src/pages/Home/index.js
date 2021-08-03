1f81804d5817a09f307becf28f6eb457c63555e7
sergjun <serrgio.madureira@gmail.com>
2021-08-03 19:33:01 -0300

    iniciada a montagem da page home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { Header } from "../../components/Header/index";
+import { Menu } from "../../components/Menu/index";
+import { Footer } from "../../components/Footer/index";
+import { Search } from "../../components/Search";
 
-const Home = () => <div>home works</div>;
-
+const Home = () => (
+  <>
+    <Header />
+    <Menu />
+    <Footer />
+    <Search />
+  </>
+);
 export default Home;
