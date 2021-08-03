d6d4844c71134866a9f2518df2b92afcdb9469a6
joaoprferreira <joaof082@gmail.com>
2021-08-03 20:48:07 -0300

    Criando api fale para montar os cards
    Signed-off-by: joaoprferreira <joaof082@gmail.com>

---
+++ src/services/api.js
@@ -0,0 +1,47 @@
+import * as I from "../assets/img";
+
+export const card = [
+  {
+    id: 1,
+    description:
+      "Vai viajar? Veja as melhores opções do que fazer com seu gato",
+    image: I.pet1,
+  },
+  {
+    id: 2,
+    description: "O Guia completo para viajar com o dog de avião",
+    image: I.pet2,
+  },
+  {
+    id: 3,
+    description: "Arrumando os Docs para voar com seu cachorro",
+    image: I.pet3,
+  },
+  {
+    id: 4,
+    description: "Partiu! Veja 12 hotéis no Brasil para levar seu pet",
+    image: I.pet4,
+  },
+  {
+    id: 5,
+    description:
+      "Quais vacinas meu Pet precisa tomar para viajar com segurança?",
+    image: I.pet5,
+  },
+  {
+    id: 6,
+    description: "Veja dicas de como levar seu pet para o intercâmbio",
+    image: I.pet6,
+  },
+  {
+    id: 7,
+    description:
+      "Vai viajar? Veja as melhores opções do que fazer com seu gato",
+    image: I.pet7,
+  },
+  {
+    id: 8,
+    description: "Arrumando os Docs para voar com seu cachorro",
+    image: I.pet8,
+  },
+];
