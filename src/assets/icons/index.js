47e33e2292db325664637a1fd4f537c7af20b446
sergjun <serrgio.madureira@gmail.com>
2021-08-03 19:28:20 -0300

    padronizada as importações dos icones

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,1 +1,6 @@
-export { default as lupaIcon } from "./lupa.svg";
+import SearchIcon from "./search-icon.svg";
+import MenuIcon from "./menu-icon.svg";
+import FacebookIcon from "./facebook-icon.svg";
+import InstagramIcon from "./instagram-icon.svg";
+
+export { FacebookIcon, InstagramIcon, SearchIcon, MenuIcon };
