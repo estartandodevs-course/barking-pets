226b61739d6080837d2e35da159696224709f143
sergjun <serrgio.madureira@gmail.com>
2021-08-03 18:18:55 -0300

    Feito a estilização das patinhas para funcionar de acordo

--- src/components/MenuDesktop/index.js
+++ src/components/MenuDesktop/index.js
@@ -17,7 +17,7 @@ export function MenuDesktop() {
           ? "menu-desktop__text--selected"
           : "menu-desktop__text--not-selected";
         return (
-          <a href="#" className={`${aClass}`}>
+          <a href="#" className={`menu-desktop__text ${aClass}`}>
             <p>{link.title}</p>
           </a>
         );
