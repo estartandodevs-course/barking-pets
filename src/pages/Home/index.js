f1f352ed0785fab545f4cf6485aeaf006920089e
sergjun <serrgio.madureira@gmail.com>
2021-08-03 23:16:04 -0300

    Melhorado a importação dos componentes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,20 @@
-import React from "react";
-import { Header } from "../../components/Header/index";
-import { Menu } from "../../components/Menu/index";
-import { Footer } from "../../components/Footer/index";
-import { Search } from "../../components/Search";
+// import React from "react";
+// import { Header } from "../../components/Header/index";
+// import { Menu } from "../../components/Menu/index";
+// import { Footer } from "../../components/Footer/index";
+// import { Search } from "../../components/Search";
 
-const Home = () => (
-  <>
-    <Header />
-    <Menu />
-    <Footer />
-    <Search />
-  </>
-);
+import * as C from "../../components/index";
+
+const Home = () => {
+  return (
+    <>
+      <C.Header />
+      <C.MenuDesktop />
+      <C.Menu />
+      <C.Footer />
+      <C.Search />
+    </>
+  );
+};
 export default Home;
