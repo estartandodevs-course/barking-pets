de2b2ecdf81209ac054fb724b361671cfe761991
joaoprferreira <joaof082@gmail.com>
2021-08-04 23:55:20 -0300

    criando os components -Hotéis mais avaliados -Veja nossas dicas na home
    Signed-off-by: joaoprferreira <joaof082@gmail.com>

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,9 @@
 import React from "react";
+import { patinhaBlack, ponto } from "../../assets/icons";
 
-import { Header, SearchComponent } from "../../components";
-import Card from "../../components/highlights";
+import { Card, Header, SearchComponent } from "../../components";
 
-import { cards } from "../../services/api";
+import { cards, hoteis } from "../../services/api";
 import "./styles.scss";
 
 const Home = () => {
@@ -11,16 +11,60 @@ const Home = () => {
     <>
       <Header />
       <SearchComponent />
-      <p>Destaques</p>
-      <div className="carrousel">
-        {cards.map(({ id, image, description }) => {
-          return (
-            <Card id={id} image={image}>
-              <p>{description}</p>
-            </Card>
-          );
-        })}
-      </div>
+      <section id="cards-hoteis">
+        <div className="title__card-section">
+          <p className="title__card-section--text">Hotéis mais avaliados</p>
+          <div className="title__card-section--linha" />
+        </div>
+        <div className="card-section">
+          {hoteis.map(({ id, name, image, nota }) => {
+            return (
+              <Card key={id} className="home__card--hotel">
+                <img src={image} alt="cachorro" className="home__card--image" />
+                <div className="home__card__footer__hotel">
+                  <p className="home__card__footer__hotel--text">
+                    {name}
+                    <a
+                      href="top"
+                      className="home__card__footer__hotel--text--link"
+                    >
+                      Saiba mais...
+                    </a>
+                  </p>
+                  <div className="home__card__footer__hotel--patinha-black-and-nota">
+                    <img
+                      src={patinhaBlack}
+                      alt="patinha"
+                      className="home__card__footer__hotel--patinha-black"
+                    />
+                    <img src={ponto} alt="ponto" />
+                    <p>{nota}</p>
+                  </div>
+                </div>
+              </Card>
+            );
+          })}
+        </div>
+      </section>
+
+      <section id="cards-animais">
+        <div className="title__card-section">
+          <p className="title__card-section--text">Veja nossas dicas</p>
+          <div className="title__card-section--linha" />
+        </div>
+        <div className="card-section">
+          {cards.map(({ id, image, description }) => {
+            return (
+              <Card key={id} className="home__card">
+                <img src={image} alt="cachorro" className="home__card--image" />
+                <div className="home__card__footer">
+                  <p className="home__card__footer--text">{description}</p>
+                </div>
+              </Card>
+            );
+          })}
+        </div>
+      </section>
     </>
   );
 };
