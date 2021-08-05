50d054d74fe190ea185360045ee9cb846bf6a9a2
joaoprferreira <joaof082@gmail.com>
2021-08-04 23:51:24 -0300

    Exportando imagens dos hoteis
    Signed-off-by: joaoprferreira <joaof082@gmail.com>

---
+++ src/assets/img/hoteis/index.js
@@ -0,0 +1,7 @@
+export { default as hotel1 } from "./hotel1.svg";
+export { default as hotel2 } from "./hotel2.svg";
+export { default as hotel3 } from "./hotel3.svg";
+export { default as hotel4 } from "./hotel4.svg";
+export { default as hotel5 } from "./hotel5.svg";
+export { default as hotel6 } from "./hotel6.svg";
+export { default as hotel7 } from "./hotel7.svg";
