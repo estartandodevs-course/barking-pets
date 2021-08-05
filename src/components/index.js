7004895289fe6b3208303fd87afbcc247bad5bda
joaoprferreira <joaof082@gmail.com>
2021-08-04 23:52:19 -0300

    Exportando component card
    Signed-off-by: joaoprferreira <joaof082@gmail.com>

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { SearchComponent } from "./Search/SearchComponent";
 export { Menu } from "./Menu";
 export { Header } from "./Header";
 export { Footer } from "./Footer";
+export { Card } from "./Card";
