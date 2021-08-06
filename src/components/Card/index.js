b867383c1a8b181da4e4c645a9cade006ba4f00d
joaoprferreira <joaof082@gmail.com>
2021-08-06 02:32:38 -0300

    passando classse como atributos para component
    Signed-off-by: joaoprferreira <joaof082@gmail.com>

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./styles.scss";
 
 export function Card({ children, ...dados }) {
-  const { containerClass, image, id } = dados;
+  const { containerClass, image, id, classImages } = dados;
 
   return (
     <div key={id} className={containerClass}>
-      <img className="card__item--image" src={image} alt="" />
+      <img className={classImages} src={image} alt="" />
       <div className="card__item--footer">{children}</div>
     </div>
   );
