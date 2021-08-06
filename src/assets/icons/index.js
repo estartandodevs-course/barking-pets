8debf9c94bbaa77c9d56f0b00bb3aca1eddbf8e5
joaoprferreira <joaof082@gmail.com>
2021-08-06 02:31:07 -0300

    Expotando o icone lupa
    Signed-off-by: joaoprferreira <joaof082@gmail.com>

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,3 +1,3 @@
-export { default as lupaIcon } from "./lupa.svg";
+export { default as lupaIcon } from "./lupaIcon.svg";
 export { default as patinhaBlack } from "./patinhaBlack.svg";
 export { default as ponto } from "./ponto.svg";
