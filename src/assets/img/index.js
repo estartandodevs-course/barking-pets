ed6c19735e144c1f4c0ae03e6809a17a1e5ce33c
sergjun <serrgio.madureira@gmail.com>
2021-08-06 12:15:40 -0300

    refatorado a importação para o formato que havia sido combinado

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -1,11 +1,20 @@
 import MenuIcon from "./menu.svg";
+import pet1 from "./pet1.svg";
+import pet2 from "./pet2.svg";
+import pet3 from "./pet3.svg";
+import pet4 from "./pet4.svg";
+import pet5 from "./pet5.svg";
+import pet6 from "./pet6.svg";
+import pet7 from "./pet7.svg";
+import pet8 from "./pet8.svg";
 
-export { MenuIcon };
-export { default as pet1 } from "./pet1.svg";
-export { default as pet2 } from "./pet2.svg";
-export { default as pet3 } from "./pet3.svg";
-export { default as pet4 } from "./pet4.svg";
-export { default as pet5 } from "./pet5.svg";
-export { default as pet6 } from "./pet6.svg";
-export { default as pet7 } from "./pet7.svg";
-export { default as pet8 } from "./pet8.svg";
+export { MenuIcon, pet1, pet2, pet3, pet4, pet5, pet6, pet7, pet8 };
+
+// export { default as pet1 } from "./pet1.svg";
+// export { default as pet2 } from "./pet2.svg";
+// export { default as pet3 } from "./pet3.svg";
+// export { default as pet4 } from "./pet4.svg";
+// export { default as pet5 } from "./pet5.svg";
+// export { default as pet6 } from "./pet6.svg";
+// export { default as pet7 } from "./pet7.svg";
+// export { default as pet8 } from "./pet8.svg";
