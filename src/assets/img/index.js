2699bfbc245b4c63f636717b9f90d2fdcdb6b794
sergjun <serrgio.madureira@gmail.com>
2021-08-07 17:50:14 -0300

    deletado os comentarios do export

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -22,12 +22,3 @@ export {
   PatinhaHeader,
   PetFooter,
 };
-
-// export { default as pet1 } from "./pet1.svg";
-// export { default as pet2 } from "./pet2.svg";
-// export { default as pet3 } from "./pet3.svg";
-// export { default as pet4 } from "./pet4.svg";
-// export { default as pet5 } from "./pet5.svg";
-// export { default as pet6 } from "./pet6.svg";
-// export { default as pet7 } from "./pet7.svg";
-// export { default as pet8 } from "./pet8.svg";
