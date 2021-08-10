973ad5b53ee39ec76031cbcb8004a4e16b66e621
joaoprferreira <joaof082@gmail.com>
2021-08-10 13:13:56 -0300

    WIP
    Signed-off-by: joaoprferreira <joaof082@gmail.com>

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -3,23 +3,42 @@ import { SearchIcon } from "../../assets/icons/index";
 import "./styles.scss";
 
 export const Search = () => {
-  const [users, setUsers] = useState([]);
   const [text, setText] = useState("");
+  const [Suggestions, setSuggestions] = useState([]);
 
-  useEffect(() => {
-    const loadUsers = async () => {
-      const response = await fetch("https://reqres.in/api/users?page=2");
-      const data = await response.json();
-      setUsers(data.data);
-    };
-    loadUsers();
-  }, []);
+  const states = [
+    {
+      id: 1,
+      UF: "RJ",
+      Estado: "Rio De Jnaeiro",
+      País: "Brasil",
+    },
+    {
+      id: 2,
+      UF: "RJ",
+      Estado: "Rio De Jnaeiro",
+      País: "Brasil",
+    },
+    {
+      id: 3,
+      UF: "RJ",
+      Estado: "Rio De Jnaeiro",
+      País: "Brasil",
+    },
+    {
+      id: 4,
+      UF: "RJ",
+      Estado: "Rio De Jnaeiro",
+      País: "Brasil",
+    },
+  ];
 
   const onChageHandle = (valueText) => {
     setText(valueText);
   };
 
-  console.log(users);
+  console.log(Suggestions);
+
   return (
     <div className="search">
       <h1>{text}</h1>
