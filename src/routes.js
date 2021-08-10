eb194dc606f692e894c6851cc6c820765d192fd4
sergjun <serrgio.madureira@gmail.com>
2021-08-09 23:45:08 -0300

    retirado o exact das outras routes

--- src/routes.js
+++ src/routes.js
@@ -9,10 +9,10 @@ export default function Routes() {
         <Route exact path="/">
           <Home />
         </Route>
-        <Route exact path="/AboutUs">
+        <Route path="/AboutUs">
           <AboutUs />
         </Route>
-        <Route exact path="/resultado-busca">
+        <Route path="/resultado-busca">
           <SearchResults />
         </Route>
       </Switch>
