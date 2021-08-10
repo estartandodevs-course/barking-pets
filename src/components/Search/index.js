a2fc6c1558032a4b05aae39d272d35f69b58d7c3
joaoprferreira <joaof082@gmail.com>
2021-08-09 21:27:41 -0300

    WIP
    Signed-off-by: joaoprferreira <joaof082@gmail.com>

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SearchIcon } from "../../assets/icons/index";
 import "./styles.scss";
 
 export const Search = () => {
+  const [users, setUsers] = useState([]);
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    const loadUsers = async () => {
+      const response = await fetch("https://reqres.in/api/users?page=2");
+      const data = await response.json();
+      setUsers(data.data);
+    };
+    loadUsers();
+  }, []);
+
+  const onChageHandle = (valueText) => {
+    setText(valueText);
+  };
+
+  console.log(users);
   return (
     <div className="search">
+      <h1>{text}</h1>
+
       <form className="search__form">
         <input
           type="text"
@@ -12,6 +31,8 @@ export const Search = () => {
           id="input"
           placeholder="Digite aqui a sua busca"
           className="search__input"
+          onChange={(e) => onChageHandle(e.target.value)}
+          value={text}
         />
         <img src={SearchIcon} alt="lupa" className="search__form--icon" />
       </form>
