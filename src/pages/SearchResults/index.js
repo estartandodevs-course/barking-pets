b002d208527856c52c25a312a12856dd9ed19172
sergjun <serrgio.madureira@gmail.com>
2021-08-13 15:31:17 -0300

    iniciada a montagem da pagina search results

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import * as C from "../../components/index";
+import { cards, hoteis } from "../../services/api";
 import "./styles.scss";
 
 const SearchResults = () => {
@@ -7,11 +8,45 @@ const SearchResults = () => {
 
   return (
     <>
+      <C.Header />
+      <C.Menu />
+      <C.MenuDesktop />
       <h3 className="search-title">
         {filter ? `Opções para ${filter}` : "  "}
       </h3>
       <C.Search />
+
       <C.Filter filter={filter} setFilter={setFilter} />
+      <section id="cards-hoteis" className="card-overflow">
+        <div className="title-section">
+          <p className="title-section--text">Resultados da pesquisa</p>
+          <div className="title-section--line" />
+        </div>
+
+        <div className="card-section">
+          {hoteis.map(({ id, name, image, nota }) => {
+            return (
+              <C.CardHotel key={id} name={name} image={image} nota={nota} />
+            );
+          })}
+        </div>
+      </section>
+
+      <section id="cards-animais" className="card-overflow">
+        <div className="title-section">
+          <p className="title-section--text">Veja nossas dicas</p>
+          <div className="title-section--line" />
+        </div>
+
+        <div className="card-section">
+          {cards.map(({ id, image, description }) => {
+            return (
+              <C.CardTips key={id} image={image} description={description} />
+            );
+          })}
+        </div>
+      </section>
+      <C.Footer />
     </>
   );
 };
