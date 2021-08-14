b9b7bcd2eefc161dc2c5231b4ca2177f660d82ac
joaoprferreira <joaof082@gmail.com>
2021-08-14 03:20:24 -0300

    Construindo mock com sugestoes para o autocomplete
    Signed-off-by: joaoprferreira <joaof082@gmail.com>

---
+++ src/services/mockLocations.js
@@ -0,0 +1,45 @@
+// 20210814025552
+// https://servicodados.ibge.gov.br/api/v1/localidades/estados
+
+export const states = [
+  {
+    id: 11,
+    sigla: "RO",
+    nome: "Rondônia",
+  },
+  {
+    id: 12,
+    uf: "AC",
+    estado: "Acre",
+
+  },
+  {
+    id: 13,
+    uf: "AM",
+    estado: "Amazonas",
+
+  },
+  {
+    id: 14,
+    uf: "RR",
+    estado: "Roraima",
+
+  },
+  {
+    id: 15,
+    uf: "PA",
+    estado: "Pará",
+
+  },
+  {
+    id: 16,
+    uf: "AP",
+    estado: "Amapá",
+
+  },
+  {
+    id: 17,
+    uf: "TO",
+    estado: "Tocantins",
+
+  }];
