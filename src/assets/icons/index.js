4e368fb84f3c00f0159cb45c6353a736227542d8
joaoprferreira <joaof082@gmail.com>
2021-08-14 03:15:30 -0300

    Adicionando icone de localização no autoComplete
    Signed-off-by: joaoprferreira <joaof082@gmail.com>

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -4,9 +4,11 @@ import FacebookIcon from "./facebook-icon.svg";
 import InstagramIcon from "./instagram-icon.svg";
 import PatinhaBlack from "./patinhaBlack.svg";
 import Ponto from "./ponto.svg";
+import locationHeart from "./location-coracao.svg";
 
 export {
   FacebookIcon,
+  locationHeart,
   InstagramIcon,
   SearchIcon,
   MenuIcon,
