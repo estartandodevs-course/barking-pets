84d0b65d1414804592e2d38ac7e88c4a073e4d87
joaoprferreira <joaof082@gmail.com>
2021-08-17 14:02:22 -0300

    Refatorando e adicionando o component button
    Signed-off-by: joaoprferreira <joaof082@gmail.com>

--- src/pages/HotelDescription/index.js
+++ src/pages/HotelDescription/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { ButtonContact } from "../../components";
 import "./styles.scss";
 
 const HotelDescription = ({ data }) => {
@@ -8,15 +9,18 @@ const HotelDescription = ({ data }) => {
 
   return (
     <section className="Full">
-      {data.filter((hotel) => idInNumber === hotel.id).map((hotel) => {
-        return (
-          <div key={hotel.id}>
-            <h1>{hotel.name}</h1>
-            <img src={hotel.image} alt={hotel.name} />
-            <p>{hotel.nota}</p>
-          </div>
-        );
-      }) }
+      {data
+        .filter((hotel) => idInNumber === hotel.id)
+        .map((hotel) => {
+          return (
+            <div key={hotel.id}>
+              <h1>{hotel.name}</h1>
+              <img src={hotel.image} alt={hotel.name} />
+              <p>{hotel.nota}</p>
+              <ButtonContact classButton="">Entrar Em Contato</ButtonContact>
+            </div>
+          );
+        })}
     </section>
   );
 };
