54c777345e42e69c0c93438d520fea1bc26bc7a6
sergjun <serrgio.madureira@gmail.com>
2021-08-16 22:37:47 -0300

    importado tudo de uma vez usando a letra S

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,21 +1,8 @@
 import { useState } from "react";
 import * as C from "../../components/index";
+import { patinhaBrown, pontoBrown } from "../../assets/icons";
 import { hoteis } from "../../services/api";
-import {
-  SearchBar,
-  searchTitle,
-  searchTotal,
-  searchTotalText,
-  searchTotalCard,
-  cardSearch,
-  cardFooter,
-  cardImage,
-  cardText,
-  cardLink,
-  cardPaw,
-  searchFooter,
-  cardFavorite,
-} from "./search.module.scss";
+import * as S from "./search.module.scss";
 import { states } from "../../services/mockLocations";
 
 const SearchResults = () => {
@@ -26,45 +13,63 @@ const SearchResults = () => {
       <C.Header />
       <C.Menu />
       <C.MenuDesktop />
-      <h3 className={searchTitle}>{filter ? `Opções para ${filter}` : "  "}</h3>
-      <C.Search classSearch={SearchBar} suggestions={states} />
+      <h3 className={S.searchTitle}>
+        {filter ? `Opções para ${filter}` : "  "}
+      </h3>
+      <C.Search classSearch={S.SearchBar} suggestions={states} />
       <C.Filter filter={filter} setFilter={setFilter} />
-      <div className={searchTotal}>
-        <p className={searchTotalText}>
+      <div className={S.searchTotal}>
+        <p className={S.searchTotalText}>
           Resultados da pesquisa:
           {hoteis.length}
         </p>
       </div>
-      <div className={searchTotalCard}>
-        {hoteis.map(({ id, name, image, nota }) => {
-          return (
-            <C.CardHotel
-              styles={{
-                container: cardSearch,
-                footer: cardFooter,
-                picture: cardImage,
-                text: cardText,
-                link: cardLink,
-                paw: cardPaw,
-              }}
-              key={id}
-              name={name}
-              image={image}
-              nota={nota}
-            >
-              <C.Favorite classFavorite={cardFavorite} />
-            </C.CardHotel>
-          );
-        })}
+      <div className={S.searchContainer}>
+        <div className={S.searchTotalCard}>
+          {hoteis.map(({ id, name, image, nota, valor }) => {
+            return (
+              <C.CardHotel
+                styles={{
+                  container: S.cardSearch,
+                  footer: S.cardFooter,
+                  picture: S.cardImage,
+                  text: S.cardText,
+                  link: S.cardLink,
+                  paw: S.cardPaw,
+                }}
+                key={id}
+                name={name}
+                image={image}
+                nota={nota}
+                valor={valor}
+                classFooter={S.searchFooter}
+              >
+                <p className={S.cardText}>{name}</p>
+                <div className={S.searchNota}>
+                  <img src={patinhaBrown} className={S.cardPaw} alt="patinha" />
+                  <img src={pontoBrown} alt="ponto" />
+                  <p className={S.cardGrade}>{nota}</p>
+                </div>
+                <div className={S.searchPrice}>
+                  <p>{valor}</p>
+                  <a href=" " target="_blank">
+                    Ver mais...
+                  </a>
+                </div>
+                <C.Favorite classFavorite={S.cardFavorite} />
+              </C.CardHotel>
+            );
+          })}
+        </div>
       </div>
 
       <C.Pagination />
-      <p className={searchTotalText}>
+      <p className={S.searchTotalText}>
         1-10 de
         {hoteis.length}
         resultados
       </p>
-      <C.Footer classFooter={searchFooter} />
+      <C.Footer classFooter={S.searchFooter} />
     </>
   );
 };
