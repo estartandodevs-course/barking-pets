fa9c4f8b95c536d8c56f14c4a8621f25513474b2
joaoprferreira <joaof082@gmail.com>
2021-08-18 16:15:30 -0300

    Limpando a hotel description
    Signed-off-by: joaoprferreira <joaof082@gmail.com>

--- src/pages/HotelDescription/index.js
+++ src/pages/HotelDescription/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Button } from "../../components";
 import { SairIcon } from "../../assets/icons";
+import { EstablishmentPrice } from "../../components";
 import { getBusinessById } from "../../services/business";
 import "./styles.scss";
 
@@ -16,31 +16,26 @@ const HotelDescription = () => {
   }, []);
 
   return (
-    <section className="Full">
-      {data
-        .filter((hotel) => idInNumber === hotel.id)
-        .map((hotel) => {
-          return (
-            <div key={hotel.id}>
-              <h1>{hotel.name}</h1>
-              <img src={hotel.image} alt={hotel.name} />
-              <p>{hotel.nota}</p>
-              <Button classButton="">Entrar Em Contato</Button>
-            </div>
-          );
-        })}
     <section className="hotelDescription">
-      {business
-        && (
-          <main key={business.id}>
-            <div className="hotelDescription__images">
-              <img src={SairIcon} alt="Sair" className="hotelDescription__image--sair" />
-              <img src={business.image} alt={business.name} className="hotelDescription__image--hotel" />
-            </div>
-            <h1>{business.name}</h1>
-            <p>{business.nota}</p>
-          </main>
-        )}
+      {business && (
+        <main key={business.id}>
+          <div className="hotelDescription__images">
+            <img
+              src={SairIcon}
+              alt="Sair"
+              className="hotelDescription__image--sair"
+            />
+            <img
+              src={business.image}
+              alt={business.name}
+              className="hotelDescription__image--hotel"
+            />
+          </div>
+          <h1>{business.name}</h1>
+          <p>{business.nota}</p>
+          <EstablishmentPrice />
+        </main>
+      )}
     </section>
   );
 };
