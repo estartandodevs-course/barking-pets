6b8464d914e3d32cebf5178003335f16f20c0468
sergjun <serrgio.madureira@gmail.com>
2021-08-18 22:11:31 -0300

    Deixado o menu funcional

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,11 +1,61 @@
-import React from "react";
-import { MenuIcon } from "../../assets/icons";
+/* eslint-disable operator-linebreak */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+
+import React, { useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import { MenuIcon, SairIcon } from "../../assets/icons";
 import "./styles.scss";
 
 export function Menu() {
+  const menuLinks = [
+    { title: "Home", path: "/" },
+    { title: "Sobre nós", path: "/sobre_nos" },
+    { title: "Dicas", path: "/dicas" },
+    { title: "Cadastre-se", path: "/entrar" },
+    { title: "Entrar", path: "/entrar" },
+  ];
+
+  const [showMenu, setShowMenu] = useState(false);
+
+  const detailsRef = useRef();
+  useEffect(() => {
+    detailsRef.current.open = false;
+  }, []);
+
   return (
-    <div className="menu">
-      <img src={MenuIcon} alt="Menu" className="menu--hamburguer" />
-    </div>
+    <>
+      <div className="menu" ref={detailsRef}>
+        <img
+          src={MenuIcon}
+          onClick={() => {
+            setShowMenu(true);
+          }}
+          alt="Menu"
+          className="menu--hamburguer"
+        />
+      </div>
+      {showMenu && (
+        <div className="menu-open">
+          <img
+            src={SairIcon}
+            onClick={() => {
+              setShowMenu(false);
+            }}
+            alt="Sair"
+            className="menu-desktop__sair"
+          />
+          {menuLinks.map((link) => {
+            return (
+              <>
+                <Link key={link.title} className="menu-mobile" to={link.path}>
+                  <p>{link.title}</p>
+                </Link>
+              </>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 }
