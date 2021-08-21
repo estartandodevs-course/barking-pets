5102870b49b034e31b45a8d1a4be6b9677303255
sergjun <serrgio.madureira@gmail.com>
2021-08-21 17:42:31 -0300

    iniciada a montagem e estilzação da pagina de resultado de busca

--- src/pages/HotelDescription/index.js
+++ src/pages/HotelDescription/index.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { SairIcon, patinhaBrown } from "../../assets/icons";
-import { BackTop, EstablishmentPrice } from "../../components";
+import {
+  SairIcon,
+  patinhaBrown,
+  pontoBrown,
+  locationHeartBrown,
+} from "../../assets/icons";
+import * as C from "../../components";
 import { getBusinessById } from "../../services/business";
-import "./styles.scss";
+import * as S from "./hotelDescription.module.scss";
 
 const HotelDescription = () => {
   const [business, setBusiness] = useState();
@@ -16,29 +21,37 @@ const HotelDescription = () => {
   }, []);
 
   return (
-    <section className="hotelDescription">
+    <section className={S.hotelDescription}>
       {business && (
         <main key={business.id}>
-          <div className="hotelDescription__images">
+          <div className={S.hotelDescriptionImages}>
             <img
               src={SairIcon}
               alt="Sair"
-              className="hotelDescription__image--sair"
+              className={S.hotelDescriptionImageSair}
             />
             <img
               src={business.image}
               alt={business.name}
-              className="hotelDescription__image--hotel"
+              className={S.hotelDescriptionImageHotel}
             />
+            <C.Favorite classFavorite={S.favoriteDescription} />
           </div>
-          <h1>{business.name}</h1>
-          <p>{business.nota}</p>
-          <p>{business.price}</p>
-          <p>{business.endereco}</p>
+          <p className={S.hotelName}>{business.name}</p>
+          <div className={S.gradeContainer}>
+            <img src={patinhaBrown} alt="Patinha" className={S.pawBrown} />
+            <img src={pontoBrown} alt="ponto" className={S.pontoBrown} />
+            <p className={S.hotelGrade}>{business.nota}</p>
+          </div>
+          <p className={S.hotelPrice}>{business.price}</p>
+          <div className={S.locationContainer}>
+            <img src={locationHeartBrown} alt="simbolo de localização marrom" />
+            <p>{business.endereco}</p>
+          </div>
           <p>{business.pais}</p>
-          <h3>Informações do hotel:</h3>
+          <p className={S.descriptionInfos}>Informações do hotel:</p>
           <p>{business.descricao}</p>
-          <h3>Comodidades:</h3>
+          <p className={S.descriptionInfos}>Comodidades:</p>
           <ul>
             {business.comodidades.map((comodidade) => {
               return (
@@ -49,9 +62,15 @@ const HotelDescription = () => {
               );
             })}
           </ul>
-          <h3>Avaliações:</h3>
-          <EstablishmentPrice price={business.price} />
-          <BackTop />
+          <p className={S.descriptionInfos}>Avaliações:</p>
+          <C.EstablishmentPrice price={business.price} />
+          <C.PublishedComment text="testando" blur />
+          <a className={S.hotelPublicationsLinks}>
+            Clique aqui para ter acesso as publicações
+          </a>
+          <C.Comment bussiness={business.name} />
+          <C.BackTop />
+          <C.Footer />
         </main>
       )}
     </section>
